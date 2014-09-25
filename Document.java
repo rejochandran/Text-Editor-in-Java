2664a746169c30747bec3aca75ab8ee22b43babe
rejochandran <rejochandran@live.com>
2014-09-25 15:22:16 +0530

    Update Document.java

--- Document.java
+++ Document.java
@@ -3,9 +3,11 @@ import java.awt.event.*;
 import javax.swing.*;
 import org.fife.ui.rtextarea.*;
 import org.fife.ui.rsyntaxtextarea.*;
+import java.io.*;
 
 public class Document extends JFrame implements ActionListener
 {
+     JFileChooser fc = new JFileChooser();
     private JTextArea ta;
     private int count;
     private JMenuBar menuBar;
@@ -16,19 +18,20 @@ public class Document extends JFrame implements ActionListener
     private JToolBar toolBar;
     public Document()
     {
-        super("Document");
+        super("Java editor by CSE 2016");
         setSize(600, 600);
         setLocationRelativeTo(null); 
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         Container pane = getContentPane();
         pane.setLayout(new BorderLayout());
 
+
+
       RSyntaxTextArea textArea = new RSyntaxTextArea(20, 60);
       textArea.setSyntaxEditingStyle(SyntaxConstants.SYNTAX_STYLE_JAVA);
       textArea.setCodeFoldingEnabled(true);
       RTextScrollPane sp = new RTextScrollPane(textArea);
       pane.add(sp);
-
 
 
         count = 0;
@@ -89,13 +92,37 @@ public class Document extends JFrame implements ActionListener
 
     setVisible(true);
 }
+void writetofile(File ff) throws Exception
+{
+            FileWriter fw = new FileWriter(ff.getAbsoluteFile());
+                 BufferedWriter bw = new BufferedWriter(fw);
+                 bw.write("works!!!");
+                 bw.close(); 
+}
+
 public void actionPerformed(ActionEvent e) 
 {
     JMenuItem choice = (JMenuItem) e.getSource();
     if (choice == saveI)
     {
-        //not yet implmented
+        
+         int returnVal = fc.showSaveDialog(Document.this);
+            if (returnVal == JFileChooser.APPROVE_OPTION) 
+            {
+                try
+                {
+                 File file = fc.getSelectedFile();
+                 //writetofile();
+             }
+             catch(Exception esa)
+             {
+
+             }
+            }
+            
+
     }
+
     else if (choice == exitI)
         System.exit(0);
     else if (choice == cutI)
